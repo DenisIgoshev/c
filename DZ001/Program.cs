d0d41d138d246968644f6bd5fd791a54a0ddbe0c
Denis <mozzg2015@gmail.com>
2023-09-24 23:41:23 +0500

    DZ!

---
+++ DZ001/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.WriteLine("Введите номер задачи: ");
+int nomer = Convert.ToInt32(Console.ReadLine());
+switch (nomer)
+{
+
+   case 1:
+   //     Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+        {
+            Console.WriteLine("Для того, чтобы узнать большее введите первое число: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            if (A > B)
+            {
+            System.Console.WriteLine($"MAX {A}");
+            }
+            else if (A < B)
+            {
+                System.Console.WriteLine($"MAX {B}");
+            }
+            else
+            {
+                System.Console.WriteLine("Цифры равны");
+            }
+            break;
+        }
+
+  case 2:
+//   Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+           {
+            Console.WriteLine("Для того, чтобы узнать большее введите первое число: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int C = Convert.ToInt32(Console.ReadLine());
+           if (A > B)
+           max = A;
+           
+
+
+
+
+           }
+
+
+
+
+
+
+
+
+
+
+
+}
