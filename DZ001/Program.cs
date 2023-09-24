b4769f20310161ef6d421164a19a112339417bdd
Denis <mozzg2015@gmail.com>
2023-09-25 01:02:31 +0500

    DZ001+1

--- DZ001/Program.cs
+++ DZ001/Program.cs
@@ -25,7 +25,7 @@ switch (nomer)
             {
                 System.Console.WriteLine("Цифры равны");
             }
-            break;
+     break;
         }
 
   case 2:
@@ -40,23 +40,50 @@ switch (nomer)
             int B = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите третье число: ");
             int C = Convert.ToInt32(Console.ReadLine());
-           if (A > B)
-           max = A;
            
+           int max = A;
 
-
-
+          if(A > max)  max = A;
+          if(B > max)  max = B;
+          if(C > max)  max = C;
+          System.Console.WriteLine($"MAX = {max}");
+       
+   break;
 
            }
+   case 3:
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
+          System.Console.WriteLine("Для того, чтобы узнать кратное введите число: ");
+          int number = Convert.ToInt32(Console.ReadLine());
+          if (number % 2 == 0)
+          {
+          System.Console.WriteLine("Кратно");
+          }
+          else
+          {
+            System.Console.WriteLine("Некратно");
+          }
 
-
-
-
-
-
-
-
-
+  break;
+   case 4:
+//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+       
+          System.Console.WriteLine("Введите число N: ");
+          int N = Convert.ToInt32(Console.ReadLine());
+          for(int i = 1; i <= N; i++)
+          {
+            if(i % 2 == 0)
+            {
+            System.Console.Write(i+ ",");
+            }
+          }
+          
+   break;
 
 }
