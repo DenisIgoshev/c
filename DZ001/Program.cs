40c4f4ebde8a30c4eaaf751db8e592e33e4fd7af
Denis <mozzg2015@gmail.com>
2023-10-02 23:51:52 +0500

    dz002

--- DZ001/Program.cs
+++ DZ001/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите номер задачи: ");
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+
+Console.WriteLine("Введите номер задачи: ");
 int nomer = Convert.ToInt32(Console.ReadLine());
 switch (nomer)
 {
@@ -85,5 +88,140 @@ switch (nomer)
           }
           
    break;
+
+
+
+// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+
+// Для остальных чисел вернуть True или False.
+case 5:
+
+Console.WriteLine("Введите пятизначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+   if(num <= 10000)
+  {
+        int division1 = num / 10000;
+        int remainder1 = num % 10;
+ 
+             if(division1 == remainder1)
+             {
+                 num = num / 10;
+               int division2 = (num / 100) % 10;
+               int remainder2 = num % 10;
+
+                if(division2 == remainder2)
+               {
+               Console.WriteLine("Палидром");          
+               }
+             } 
+             else 
+             Console.WriteLine("Не палидром");
+             
+            
+   }
+     else
+     {
+     Console.WriteLine("Некорректное число!");
+     }
+
+
+
+break;
+
+
+case 6:
+
+// Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+
+break;
+
+case 7:
+// Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+
+// N = 3 
+// /*
+// 1
+// 8
+// 27
+// */
+
+// N = 5
+// /*
+// 1
+// 8
+// 27
+// 64
+// 125
+// */
+
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.WriteLine(coll[index]+  " " );
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
+
+
+
+
+
+
+
+break;
 
 }
