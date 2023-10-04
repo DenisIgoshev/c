2c9a5453df84804095978d0e593fbcf21ebd36b8
Denis <mozzg2015@gmail.com>
2023-10-04 21:16:40 +0500

    pra5002

---
+++ pra5002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+FillArray(arr);
+System.Console.WriteLine(string.Join(", ", arr));
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] *= -1;
+}
+
+System.Console.WriteLine(string.Join("; ", arr));
+
+
+
