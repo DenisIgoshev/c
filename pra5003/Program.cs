601cb8fa3ccb6546c5b790e4497ae942d0dc9565
Denis <mozzg2015@gmail.com>
2023-10-04 21:17:18 +0500

    pra5003

---
+++ pra5003/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArray(arr);
+System.Console.WriteLine(string.Join(", ", arr));
+
+System.Console.Write("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool FinD = false;
+
+for (int i=0; i<arr.Length;i++)
+{
+   if (arr[i]==number) 
+   {
+    FinD = true;
+    break;
+   }
+}
+System.Console.WriteLine(FinD);
